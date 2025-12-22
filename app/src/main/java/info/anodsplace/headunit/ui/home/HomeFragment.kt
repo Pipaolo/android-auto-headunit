8334e60719df1450a7ff7c68dc94befa1e7c04da
paotea <paolo@digitzs.com>
2025-12-22 14:37:53 +0800

    feat(home): add UsbReceiver.Listener interface to HomeFragment

--- app/src/main/java/info/anodsplace/headunit/ui/home/HomeFragment.kt
+++ app/src/main/java/info/anodsplace/headunit/ui/home/HomeFragment.kt
@@ -1,18 +1,36 @@
 package info.anodsplace.headunit.ui.home
 
+import android.app.AlertDialog
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import info.anodsplace.headunit.App
 import info.anodsplace.headunit.R
+import info.anodsplace.headunit.aap.AapService
+import info.anodsplace.headunit.connection.UsbAccessoryMode
+import info.anodsplace.headunit.connection.UsbReceiver
+import info.anodsplace.headunit.connection.isInAccessoryMode
+import info.anodsplace.headunit.connection.statusText
+import info.anodsplace.headunit.connection.uniqueName
 import info.anodsplace.headunit.ui.settings.SettingsActivity
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), UsbReceiver.Listener {
 
     private var usbStatusText: TextView? = null
+    private lateinit var usbManager: UsbManager
+    private var usbReceiver: UsbReceiver? = null
+    private var pendingDevice: UsbDevice? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -56,6 +74,29 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         usbStatusText = null
+    }
+
+    // UsbReceiver.Listener implementation
+    override fun onUsbDetach(device: UsbDevice) {
+        // Device disconnected - update UI if needed
+        pendingDevice = null
+    }
+
+    override fun onUsbAttach(device: UsbDevice) {
+        // Device attached - could refresh list if dialog is open
+    }
+
+    override fun onUsbPermission(granted: Boolean, connect: Boolean, device: UsbDevice) {
+        if (granted && connect) {
+            connectToDevice(device)
+        } else {
+            updateUsbStatus(getString(R.string.usb_status_ready))
+            pendingDevice = null
+        }
+    }
+
+    private fun connectToDevice(device: UsbDevice) {
+        // Will be implemented in next task
     }
 
     companion object {
